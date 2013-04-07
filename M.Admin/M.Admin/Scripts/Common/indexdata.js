d35c71577c72cf9716e5fbf7bfd6411ff94c3031
kidlg <kidlg@foxmail.com>
2013-04-07 18:23:02 +0800

    Create REPO

---
+++ M.Admin/M.Admin/Scripts/Common/indexdata.js
@@ -0,0 +1,29 @@
+﻿var indexdata =
+[
+    { text: '首页', isexpand: false, children: [
+		{ url: "/home/welcome", text: "欢迎" }
+	]
+    }
+];
+
+
+var indexdata2 =
+[
+    { isexpand: "true", text: "表格", children: [
+        { isexpand: "true", text: "可排序", children: [
+		    { url: "dotnetdemos/grid/sortable/client.aspx", text: "客户端" },
+            { url: "dotnetdemos/grid/sortable/server.aspx", text: "服务器" }
+	    ]
+        },
+        { isexpand: "true", text: "可分页", children: [
+		    { url: "dotnetdemos/grid/pager/client.aspx", text: "客户端" },
+            { url: "dotnetdemos/grid/pager/server.aspx", text: "服务器" }
+	    ]
+        },
+        { isexpand: "true", text: "树表格", children: [
+		    { url: "dotnetdemos/grid/treegrid/tree.aspx", text: "树表格" }
+	    ]
+        }
+    ]
+    }
+];
